aebb25ee00084754153b689944fa2e90d27176b2
cxzhong <chexin.zhong@outlook.com>
2025-09-13 20:57:48 +0800

    fix: Update version and commit ID in _version.py to reflect latest changes

--- g6k/_version.py
+++ g6k/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev83+gbb5538a6e.d20250913'
-__version_tuple__ = version_tuple = (0, 1, 'dev83', 'gbb5538a6e.d20250913')
+__version__ = version = '0.1.dev102+g89be8b809.d20250913'
+__version_tuple__ = version_tuple = (0, 1, 'dev102', 'g89be8b809.d20250913')
 
-__commit_id__ = commit_id = 'gbb5538a6e'
+__commit_id__ = commit_id = 'g89be8b809'
